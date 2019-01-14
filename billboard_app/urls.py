83552032d39320247ebd29317a6cfba7bafc4bc7
Rebecca Kawa <reb.kawa@gmail.com>
2019-01-15 00:51:31 +0200

    first commit

--- billboard_app/urls.py
+++ billboard_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.login_form, name='login'),
+    url(r'^logout/$', views.logout_form, name='logout'),
 ]
--- billboard_app/views.py
+++ billboard_app/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 from django.views.decorators.csrf import csrf_exempt
 import datetime
-
 
 @csrf_exempt
 def index(request):
@@ -23,3 +24,36 @@ def index(request):
     context['date'] = date
     print(context)
     return render(request, 'billboard_app/posts.html', context)
+
+
+def login_form(request):
+    if request.method == 'GET':
+        return render(request, "registration/login.html")
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        print("hello")
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/billboard/')
+        else:
+            return redirect('/billboard/login/')
+
+
+def logout_form(request):
+    logout(request)
+    return render(request, 'registration/logout.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return index(request)
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", { "form": form })
